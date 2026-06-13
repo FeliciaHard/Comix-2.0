6bbd535bd9831f41ddc074a00d7927030aaaa127
FeliciaHard <feliciahardy505@gmail.com>
2026-06-13 15:57:08 +0800

    Add files via upload

--- app/signin/layout.tsx
+++ app/signin/layout.tsx
@@ -1,12 +1,11 @@
 import type { Metadata } from "next";
 import { Providers } from "@/components/provider";
+import SigninBackground from "./signin-background";
 
 const appName = process.env.APP_NAME ?? "MyApp";
 
 function capitalizeFirstLetter(word?: string) {
-  if (typeof word !== 'string' || word.length === 0) {
-    return ""; // Handle undefined, null, or empty string
-  }
+  if (typeof word !== "string" || word.length === 0) return "";
   return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
@@ -21,24 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="relative bg-white dark:bg-black text-black dark:text-white">
-        <div
-            className="
-                bg-[url('https://feliciahard.github.io/comix-src/images/type3.webp')]
-                bg-cover sm:bg-center bg-no-repeat bg-brightness-[0.2] grayscale
-                lg:bg-none
-            "
-        >
-            {/* Gradient overlay */}
-            <div className="absolute inset-0 bg-gradient-to-b from-black/40 to-black/80" />
-
-            {/* Content sits above the gradient */}
-            <div className="relative z-10">
-            <Providers>
-                {children}
-            </Providers>
-            </div>
-        </div>
-    </div>
+    <SigninBackground>
+      <Providers>{children}</Providers>
+    </SigninBackground>
   );
 }
